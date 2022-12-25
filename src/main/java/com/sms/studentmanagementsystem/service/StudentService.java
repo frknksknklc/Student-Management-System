ca000d3465fa2efde7af087ad0a62acee7c29be0
Furkan Keskinkilic <furkankeskinkilic97@gmail.com>
2022-12-25 17:18:20 +0300

    update and delete added

--- src/main/java/com/sms/studentmanagementsystem/service/StudentService.java
+++ src/main/java/com/sms/studentmanagementsystem/service/StudentService.java
@@ -1,6 +1,9 @@
 package com.sms.studentmanagementsystem.service;
 
 import java.util.List;
+import java.util.function.LongFunction;
+
+import org.hibernate.engine.jdbc.connections.spi.AbstractDataSourceBasedMultiTenantConnectionProviderImpl;
 
 import com.sms.studentmanagementsystem.entity.Student;
 
@@ -8,4 +11,9 @@ public interface StudentService {
 		List<Student> getAllStudents();
 		
 		Student saveStudent(Student student);
+		
+		Student getStudentById(Long id);
+		Student updateStudent(Student student);
+		
+		void deleteStudentById(Long id);
  }
